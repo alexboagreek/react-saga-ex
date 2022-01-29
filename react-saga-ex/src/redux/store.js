853d3d0ba13290a8d8dc6bd26b3f725b920e32b8
Alex <olivesargreek@gmail.com>
2022-01-29 16:35:23 +0300

    create: redux store

---
+++ react-saga-ex/src/redux/store.js
@@ -0,0 +1,16 @@
+import { createStore, compose } from "redux";
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const configureStore = preloadedState => createStore (
+    reducer,
+    preloadedState,
+    composeEnhancers(),
+);
+
+const store = configureStore( {} );
+
+export default store;
