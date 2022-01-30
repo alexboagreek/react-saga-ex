4026d59d3530a775636cd8d7c3bb8e5336be9251
Alex <olivesargreek@gmail.com>
2022-01-30 09:36:09 +0300

    feat: take effect

--- react-saga-ex/src/redux/sagas/index.js
+++ react-saga-ex/src/redux/sagas/index.js
@@ -1,4 +1,18 @@
+import { take } from "@redux-saga/core/effects";
+import { INCREASE_COUNT, DECREASE_COUNT } from '../constants';
+
+
+export function* workerSaga() {
+
+}
+
+export function* watchClickSaga() {
+    yield take(INCREASE_COUNT);
+    yield take(DECREASE_COUNT);
+
+}
+ 
+
 export default function* rootSaga() {
-    console.log('React Saga!');
-    yield;
+    yield watchClickSaga();
 }
