b56f853087c16af0059790b8976c8298b836034e
Alex <olivesargreek@gmail.com>
2022-01-30 10:36:32 +0300

    feat: test workerSaga

--- react-saga-ex/src/redux/actions/actionCreator.js
+++ react-saga-ex/src/redux/actions/actionCreator.js
@@ -1,6 +1,5 @@
 import { INCREASE_COUNT, DECREASE_COUNT } from "../constants";
 
-
 export const increaseCount = () => ({
     type: INCREASE_COUNT,
 });
--- react-saga-ex/src/redux/sagas/index.js
+++ react-saga-ex/src/redux/sagas/index.js
@@ -1,15 +1,22 @@
-import { take } from "@redux-saga/core/effects";
+import { takeEvery, takeLatest, takeLeading, select } from "@redux-saga/core/effects";
 import { INCREASE_COUNT, DECREASE_COUNT } from '../constants';
 
+const delay = (time) => new Promise((resolve, reject) => {
+    setTimeout(resolve, time * 1000);
+})
 
 export function* workerSaga() {
-
+    const count = yield select(({ counter }) => counter.count);
+    yield delay(2);
+    console.log(`request ${count}`);
+ 
 }
 
 export function* watchClickSaga() {
-    yield take(INCREASE_COUNT);
-    yield take(DECREASE_COUNT);
-
+    yield takeEvery(INCREASE_COUNT, workerSaga);
+    // yield take(DECREASE_COUNT);
+    yield takeLeading(INCREASE_COUNT, workerSaga);
+ 
 }
  
 
